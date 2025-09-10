1b5d318a0630ba929503e3f3bb8f1750a42d0c2d
GONOR\gonor <gustav.osterdahl99@gmail.com>
2025-09-10 16:30:00 +0200

    fixed logstar

--- Lab2/uppgift1.py
+++ Lab2/uppgift1.py
@@ -1,9 +1,12 @@
 
 import math
 def logStar(x):
-    if math.log(x,2)<x:
+    if math.log(x,2)<1:
         return 1
     elif math.log(x,2)==1:
         return 1
     else:
         return logStar(math.log(x,2))+1
+    
+
+print(logStar(1024))
